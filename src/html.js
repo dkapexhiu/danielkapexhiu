9c2aa8ebcc7191c89ec3d3b2b9e9892833b37fe0
Daniel <daniel.kapexhiu@gmail.com>
2019-09-23 15:23:02 +0200

    Update html.js

--- src/html.js
+++ src/html.js
@@ -13,6 +13,21 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+     <script
+     dangerouslySetInnerHTML={{
+        __html=`
+ var _foxpush = _foxpush || []; 
+ _foxpush.push(['_setDomain', 'danielkapexhiucom']); 
+ (function(){ 
+ var foxscript = document.createElement('script'); 
+ foxscript.src = '//cdn.foxpush.net/sdk/foxpush_SDK_min.js'; 
+ foxscript.type = 'text/javascript'; 
+ foxscript.async = 'true'; 
+ var fox_s = document.getElementsByTagName('script')[0]; 
+ fox_s.parentNode.insertBefore(foxscript, fox_s);})(); 
+   	 `
+     }}
+     />,
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
