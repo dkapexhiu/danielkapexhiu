130854a8bae369cd4bd4f6df1374b16d97db98c3
Daniel <daniel.kapexhiu@gmail.com>
2019-09-23 16:25:54 +0200

    Update html.js

--- src/html.js
+++ src/html.js
@@ -13,7 +13,19 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-     <script
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <noscript key="noscript" id="gatsby-noscript">
+          This app works best with JavaScript enabled.
+        </noscript>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+	  <script
      dangerouslySetInnerHTML={{
         __html=`
 	    (function(p,u,s,h){
@@ -29,18 +41,6 @@ export default function HTML(props) {
    	 `
      }}
      />
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
       </body>
     </html>
   )
