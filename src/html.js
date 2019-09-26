1b3b664a385b282c682d26b5e7127a15e9552817
Daniel <daniel.kapexhiu@gmail.com>
2019-09-26 15:47:22 +0200

    Update html.js

--- src/html.js
+++ src/html.js
@@ -26,9 +26,10 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-    	<Helmet><script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script><script>
+    	<script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script>
+	<Helmet><script>
 	    {`var subscribersSiteId='3b59f502-b7d3-4a3e-91e2-a63c3b5e5ff1';`}
-      </script></Helmet>
+     	</script></Helmet>
       </body>
     </html>
   )
